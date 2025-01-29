adbc81eb49880bcf654a821bdf07fefb2fc5ffc7
benitto-cartrabbit <benitto@cartrabbit.in>
2025-01-29 12:23:27 +0530

    removed unused logics and files

--- Core/routes/admin-hooks.php
+++ Core/routes/admin-hooks.php
@@ -8,14 +8,8 @@ use RelayWp\Affiliate\App\Hooks\AdminHooks;
 use RelayWp\Affiliate\Core\Models\CoreModel;
 
 $admin_hooks = [
-    'actions' => [
-        'admin_init' => ['callable' => [AdminHooks::class, 'init'], 'priority' => 10, 'accepted_args' => 1],
-        'admin_head' => ['callable' => [AdminHooks::class, 'head'], 'priority' => 10, 'accepted_args' => 1],
-        'admin_menu' => ['callable' => [AdminHooks::class, 'addMenu'], 'priority' => 10, 'accepted_args' => 1],
-    ],
-    'filters' => [
-        'rwpa_affiliate_get_models' => ['callable' => [CoreModel::class, 'getCoreModels'], 'priority' => 10, 'accepted_args' => 1],
-    ],
+    'actions' => [],
+    'filters' => [],
 ];
 
 $store_front_hooks = [
